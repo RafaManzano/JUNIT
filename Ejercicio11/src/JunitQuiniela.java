2c5d6644420d49968c0d22bc2ddf2f57c9184bdb
Rafael Manzano <rafamanzanomedina@gmail.com>
2019-06-03 14:39:10 +0200

    Initial commit

---
+++ Ejercicio11/src/JunitQuiniela.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JunitQuiniela {
+
+	@Test
+	public void QuinielaAcertada() {
+		char[] a1 = {'1','1','1','1','1','1','1','1','1','1','1','1','1','1'};
+		char[] a2 = {'1','1','1','1','1','1','1','1','1','1','1','1','1','1'};
+	}
+
+}
